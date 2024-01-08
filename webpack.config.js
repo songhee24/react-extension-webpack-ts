85d907869dc97b4fd94a50a648919e6f61072606
songhee24 <akaratel61@gmail.com>
2024-01-08 15:16:37 +0600

    avoid js problem

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,17 @@ const path = require("path");
 const CopyPlugin = require("copy-webpack-plugin");
 const HtmlWebPlugin = require("html-webpack-plugin");
 
+const getHtmlPlugins = (chunks) => {
+  return chunks.map(
+    (chunk) =>
+      new HtmlWebPlugin({
+        title: "React Extension",
+        filename: `${chunk}.html`,
+        chunks: [chunk],
+      })
+  );
+};
+
 module.exports = {
   mode: "development",
   devtool: "cheap-module-source-map",
@@ -35,15 +46,4 @@ module.exports = {
     filename: "[name].js",
     path: path.resolve(__dirname, "dist"),
   },
-};
-
-const getHtmlPlugins = (chunks) => {
-  return chunks.map(
-    (chunk) =>
-      new HtmlWebPlugin({
-        title: "React Extension",
-        filename: `${chunk}.html`,
-        chunks: [chunk],
-      })
-  );
 };
