c3ebc3e4f8cc727147e9fa93d90b5a5c93512f00
songhee24 <akaratel61@gmail.com>
2024-01-09 17:12:53 +0600

    upd options

--- src/options/options.tsx
+++ src/options/options.tsx
@@ -2,9 +2,15 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import "./options.css";
 
-const popup = <p>Hello World</p>;
+const App: React.FC<{}> = () => {
+  return (
+    <div>
+      <img src="icon.png" />
+    </div>
+  );
+};
 
 const root = document.createElement("div");
 document.body.append(root);
 const container = createRoot(root);
-container.render(popup);
+container.render(<App />);
