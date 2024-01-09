0d9ee3fa16c420c46a8b7180af9d9c243129ecba
songhee24 <akaratel61@gmail.com>
2024-01-09 17:37:40 +0600

    typing weather response

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,6 +1,28 @@
 const OPEN_WEATHER_API_KEY = "396f40c68c5687dfeeda306c4d8280ab";
 
-export async function fetchWeatherData(city: string): Promise<any> {
+export interface OpenWeatherData {
+  name: string;
+  main: {
+    feels_like: number;
+    humidity: number;
+    pressure: number;
+    temp: number;
+    temp_max: number;
+    temp_min: number;
+  };
+  weather: {
+    description: string;
+    icon: string;
+    id: number;
+    main: string;
+  }[];
+  wind: {
+    deg: number;
+    speed: number;
+  };
+}
+
+export async function fetchWeatherData(city: string): Promise<OpenWeatherData> {
   const res = await fetch(
     `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${OPEN_WEATHER_API_KEY}`
   );
@@ -8,6 +30,6 @@ export async function fetchWeatherData(city: string): Promise<any> {
     throw new Error("Something Went Wrong");
   }
 
-  const data = await res.json();
+  const data: OpenWeatherData = await res.json();
   return data;
 }
