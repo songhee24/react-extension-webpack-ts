288c1aa18c668884d8b51c464eb80f86094ffe9f
songhee24 <akaratel61@gmail.com>
2024-01-09 16:29:01 +0600

    update webpack import common config

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,5 +1,5 @@
 const { merge } = require("webpack-merge");
-const commonWebpackConfig = require("webpack.common");
+const commonWebpackConfig = require("./webpack.common.js");
 module.exports = merge(commonWebpackConfig, {
   mode: "development",
   devtool: "cheap-module-source-map",
--- webpack.prod.js
+++ webpack.prod.js
@@ -1,5 +1,5 @@
 const { merge } = require("webpack-merge");
-const commonWebpackConfig = require("webpack.common");
+const commonWebpackConfig = require("./webpack.common.js");
 module.exports = merge(commonWebpackConfig, {
   mode: "production",
 });
