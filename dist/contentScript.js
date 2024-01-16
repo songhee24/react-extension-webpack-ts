e04049d9766e0fc7099ef1bac24b4c155404b5f5
songhee24 <akaratel61@gmail.com>
2024-01-16 22:20:14 +0600

    css

--- dist/contentScript.js
+++ dist/contentScript.js
@@ -9722,7 +9722,15 @@ __webpack_require__.r(__webpack_exports__);
 
 var ___CSS_LOADER_EXPORT___ = _node_modules_css_loader_dist_runtime_api_js__WEBPACK_IMPORTED_MODULE_1___default()((_node_modules_css_loader_dist_runtime_sourceMaps_js__WEBPACK_IMPORTED_MODULE_0___default()));
 // Module
-___CSS_LOADER_EXPORT___.push([module.id, ``, "",{"version":3,"sources":[],"names":[],"mappings":"","sourceRoot":""}]);
+___CSS_LOADER_EXPORT___.push([module.id, `.overlayCard {
+    position: fixed;
+    left: 5%;
+    top: 15%;
+    max-width: 240px;
+    max-height: 240px;
+    z-index: 99999;
+}
+`, "",{"version":3,"sources":["webpack://./src/contentScript/contentScript.css"],"names":[],"mappings":"AAAA;IACI,eAAe;IACf,QAAQ;IACR,QAAQ;IACR,gBAAgB;IAChB,iBAAiB;IACjB,cAAc;AAClB","sourcesContent":[".overlayCard {\n    position: fixed;\n    left: 5%;\n    top: 15%;\n    max-width: 240px;\n    max-height: 240px;\n    z-index: 99999;\n}\n"],"sourceRoot":""}]);
 // Exports
 /* harmony default export */ const __WEBPACK_DEFAULT_EXPORT__ = (___CSS_LOADER_EXPORT___);
 
