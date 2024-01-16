ea86c63514aec665d08326a37ba0a76a201b6990
songhee24 <akaratel61@gmail.com>
2024-01-16 22:16:58 +0600

    inject card

--- src/contentScript/contentScript.tsx
+++ src/contentScript/contentScript.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
+import WeatherCard from "../components/WeatherCard";
 
 chrome.runtime.sendMessage("From the content script", (response) => {
   console.log(response);
 });
 
+const App: React.FC<{}> = () => {
+  return <WeatherCard city={"Osh"} tempScale={"metric"} />;
+};
+
 const root = document.createElement("div");
 document.body.append(root);
 const container = createRoot(root);
-container.render(<h1>Hello World</h1>);
+container.render(<App />);
