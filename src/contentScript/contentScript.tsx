af97c5e15498b5971089adb17546cad8cef1377b
songhee24 <akaratel61@gmail.com>
2024-01-16 22:13:38 +0600

    inject hello world

--- src/contentScript/contentScript.tsx
+++ src/contentScript/contentScript.tsx
@@ -1,4 +1,11 @@
-console.log("content script running");
+import React from "react";
+import { createRoot } from "react-dom/client";
+
 chrome.runtime.sendMessage("From the content script", (response) => {
   console.log(response);
 });
+
+const root = document.createElement("div");
+document.body.append(root);
+const container = createRoot(root);
+container.render(<h1>Hello World</h1>);
